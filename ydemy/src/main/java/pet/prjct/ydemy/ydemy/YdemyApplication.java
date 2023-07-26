497fb213204bc0eab4f02d4137df8fc973f9a182
NazariyBrynyarskiy <92336184+NazariyBrynyarskiy@users.noreply.github.com>
2023-07-26 14:08:26 +0300

    Add files via upload

---
+++ ydemy/src/main/java/pet/prjct/ydemy/ydemy/YdemyApplication.java
@@ -0,0 +1,13 @@
+package pet.prjct.ydemy.ydemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YdemyApplication.class, args);
+	}
+
+}
